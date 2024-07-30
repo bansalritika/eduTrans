4b78158844a28ffa9d5ba78387a14252d84b5060
Niharika_Attri <niharikaattri2005@gmail.com>
2024-07-31 01:08:26 +0530

    user controlller

--- API/app.js
+++ API/app.js
@@ -1,3 +1,23 @@
 const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+const app = express()
+
+mongoose.connect(process.env.mongodbURI)
+    .then(
+        app.listen(3333, () => {
+            console.log('connected to database');
+            console.log('server connected to port 3333');
+        })
+    ).catch(
+        (err) => {console.log(err);}
+    )
+
+app.use(async(req, res)=>{
+    res.status(404).json({
+        message: "page not found"
+    })
+})
